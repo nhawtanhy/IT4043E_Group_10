1901182cf161601c63c5672072aa922c7fd88e7b
MinhmpcNguyen <ntm.cmv@gmail.com>
2025-12-27 21:47:59 +0700

    scale S3

--- kubernetes/s3-history/app/s3_weather_history.py
+++ kubernetes/s3-history/app/s3_weather_history.py
@@ -13,14 +13,78 @@ from botocore.exceptions import ClientError
 S3_BUCKET = os.getenv("S3_BUCKET_NAME", "hust-bucket-storage")
 SILVER_PREFIX = "weather_silver"
 
-CITY_LIST = ["Hanoi", "Ho Chi Minh City", "Da Nang", "Haiphong", "Can Tho"]
+CITY_LIST = [
+    "An Giang",
+    "Bac Ninh",
+    "Ca Mau",
+    "Cao Bang",
+    "Can Tho",
+    "Da Nang",
+    "Buon Ma Thuot",  # approximate Đắk lắk
+    "Dien Bien Phu",  # approximate Điện Biên
+    "Can Gio",  # approximate Đồng Nai
+    "Cao Lanh",  # approximate Đồng Tháp
+    "Gia Lai",
+    "Hanoi",
+    "Hà Tĩnh",
+    "Haiphong",
+    "Ho Chi Minh City",
+    "Hung Yen",
+    "Khánh Hòa",
+    "Lai Chau",
+    "Da Lat",  # approximate Lâm Đồng
+    "Lang Son",
+    "Lao Cai",
+    "Vinh",  # approximate Nghệ An
+    "Ninh Binh",
+    "Phu Tho",
+    "Quang Ngai",
+    "Cam Pha Mines",  # approximate Quảng Ninh
+    "Quảng Trị",
+    "Son La",
+    "Tay Ninh",
+    "Thai Nguyen",
+    "Thanh Hoa",
+    "Hue",
+    "Tuyen Quang",
+    "Vinh Long",
+]
 
 CITY_COORDS = {
-    "Hanoi": {"lat": 21.0285, "lon": 105.8542},
-    "Ho Chi Minh City": {"lat": 10.8231, "lon": 106.6297},
-    "Da Nang": {"lat": 16.0544, "lon": 108.2022},
-    "Haiphong": {"lat": 20.8449, "lon": 106.6881},
-    "Can Tho": {"lat": 10.0452, "lon": 105.7469},
+    "An Giang": {"lat": 10.5, "lon": 105.1667},
+    "Bac Ninh": {"lat": 21.1833, "lon": 106.05},
+    "Ca Mau": {"lat": 9.1769, "lon": 105.15},
+    "Cao Bang": {"lat": 22.6667, "lon": 106.25},
+    "Can Tho": {"lat": 10.0333, "lon": 105.7833},
+    "Da Nang": {"lat": 16.0678, "lon": 108.2208},  # Turan
+    "Buon Ma Thuot": {"lat": 12.6667, "lon": 108.05},
+    "Dien Bien Phu": {"lat": 21.3833, "lon": 103.0167},
+    "Can Gio": {"lat": 10.4167, "lon": 106.9667},
+    "Cao Lanh": {"lat": 10.45, "lon": 105.6333},
+    "Gia Lai": {"lat": 13.75, "lon": 108.25},
+    "Hanoi": {"lat": 21.0245, "lon": 105.8412},
+    "Hà Tĩnh": {"lat": 18.3453, "lon": 105.9019},
+    "Haiphong": {"lat": 20.8561, "lon": 106.6822},
+    "Ho Chi Minh City": {"lat": 10.75, "lon": 106.6667},
+    "Hung Yen": {"lat": 20.65, "lon": 106.0667},
+    "Khánh Hòa": {"lat": 10.6765, "lon": 105.1903},
+    "Lai Chau": {"lat": 22.3997, "lon": 103.4517},
+    "Da Lat": {"lat": 11.9465, "lon": 108.4419},
+    "Lang Son": {"lat": 21.8333, "lon": 106.7333},
+    "Lao Cai": {"lat": 22.4833, "lon": 103.95},
+    "Vinh": {"lat": 18.6667, "lon": 105.6667},
+    "Ninh Binh": {"lat": 20.2539, "lon": 105.975},
+    "Phu Tho": {"lat": 21.3988, "lon": 105.227},
+    "Quang Ngai": {"lat": 15.1167, "lon": 108.8},
+    "Cam Pha Mines": {"lat": 21.0167, "lon": 107.3},
+    "Quảng Trị": {"lat": 16.75, "lon": 107.2},
+    "Son La": {"lat": 21.3167, "lon": 103.9},
+    "Tay Ninh": {"lat": 11.3, "lon": 106.1},
+    "Thai Nguyen": {"lat": 21.5928, "lon": 105.8442},
+    "Thanh Hoa": {"lat": 19.8, "lon": 105.7667},
+    "Hue": {"lat": 16.4667, "lon": 107.6},
+    "Tuyen Quang": {"lat": 21.8233, "lon": 105.2181},
+    "Vinh Long": {"lat": 10.25, "lon": 105.9667},
 }
 
 s3 = boto3.client("s3")
